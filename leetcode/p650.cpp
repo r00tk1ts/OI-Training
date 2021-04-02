f2a865e37d1735db7da82dd37812a1ac062cf36d
r00tk1ts <whereisr00tk1t@gmail.com>
2021-04-02 23:57:05 +0800

    update

---
+++ leetcode/p650.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minSteps(int n) {}
+};
+
+int main() {
+  int n;
+  cin >> n;
+  Solution s;
+  cout << s.minSteps(n) << endl;
+  return 0;
+}
